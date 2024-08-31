d5daae55d333258ae568612ff3c82f672c1c1481
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-08-31 05:25:53 -0400

    added comments for Platformer for game algorithm.

--- src/main/java/testdomequestions/Platformer.java
+++ src/main/java/testdomequestions/Platformer.java
@@ -3,7 +3,23 @@ package testdomequestions;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+/*
+Gaming algorithm...
+when a player uses method jumpLeft(), the player should move 2 spaces to the left,
+and also remove the previous index in the array.
+When a player uses method jumpRight(), the player should move 2 spaces to the right,
+and also remove the previous index.
 
+Example:
+given array {0,1,2,3,4,5} with current index = 3;
+jumpLeft should produce the following results:
+current index = 1 (value is 1)
+new array values {0,[1],2,4,5}
+
+then, jumpRight should produce the following results:
+current index = 2 (value is 4)
+new array values {0,2,[4],5}
+ */
 public class Platformer {
     private static Integer currentIndex = 3;
     private static final LinkedList<Integer> floorMap = new LinkedList<>();
