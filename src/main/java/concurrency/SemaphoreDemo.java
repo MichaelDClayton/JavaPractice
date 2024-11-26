1870926ebd9dddd65aea20f0321916db53063cd1
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-11-26 12:23:20 -0500

    Semaphore added.

---
+++ src/main/java/concurrency/SemaphoreDemo.java
@@ -0,0 +1,46 @@
+package concurrency;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(1);
+        Thread one = new Thread(new MyConsumer(semaphore, "Thread 1", 2000));
+        one.start();
+
+        Thread two = new Thread(new MyConsumer(semaphore, "Thread 2", 0));
+        two.start();
+
+
+    }
+}
+
+class MyConsumer implements Runnable{
+    private final Semaphore semaphore;
+    private final String threadName;
+    private final long sleepTime;
+
+    public MyConsumer(Semaphore semaphore, String threadName, long sleepTime) {
+        this.semaphore = semaphore;
+        this.threadName = threadName;
+        this.sleepTime = sleepTime;
+    }
+
+    @Override
+    public void run(){
+        System.out.println(this.threadName+" is attempting to get permit from semaphore.");
+        try {
+            this.semaphore.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Permit acquired by "+this.threadName);
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(this.threadName+" is releasing semaphore.");
+        semaphore.release();
+    }
+}
--- src/main/java/concurrency/executor/ExecutorServiceDemo.java
+++
@@ -1,9 +0,0 @@
-package concurrency.executor;
-
-public class ExecutorServiceDemo {
-    public static void main(String[] args) {
-
-    }
-}
-
-
