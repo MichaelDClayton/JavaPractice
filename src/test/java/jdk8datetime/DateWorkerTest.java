a479f7a33f5192b019292c614567ba2a6fb1138a
Michael D Clayton <michael.d.clayton@hotmail.com>
2024-12-13 16:13:28 -0500

    Updated DateWorkerTest.java to use only new java.time.* classes.

--- src/test/java/jdk8datetime/DateWorkerTest.java
+++ src/test/java/jdk8datetime/DateWorkerTest.java
@@ -6,35 +6,28 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+
 
 @RunWith(JUnit4.class)
 public class DateWorkerTest extends TestCase {
 
     @Test
     public void test_LocalDate() {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        Integer day = localDateTime.getDayOfMonth();
-        Integer month = localDateTime.getMonthValue();
-        Integer year = localDateTime.getYear();
-
-        Integer dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-        Integer monthOfYear = Calendar.getInstance().get(Calendar.MONTH) + 1;
-        Integer calYear = Calendar.getInstance().get(Calendar.YEAR);
-        assertEquals(day, dayOfMonth);
-        assertEquals(month, monthOfYear);
-        assertEquals(year, calYear);
-
+        LocalDate localDate = LocalDate.of(2024, 12, 13);
+        assertEquals(13, localDate.getDayOfMonth());
+        assertEquals(2024, localDate.getYear());
+        assertEquals(12, localDate.getMonthValue());
+        assertEquals(12, Month.DECEMBER.getValue());
     }
 
     @Test
     public void test_LocalDateString() {
-        String yearString = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-        LocalDate localDate = LocalDate.now();
+        LocalDate localDate = LocalDate.of(2024, 12, 13);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String formattedDate = localDate.format(formatter);
-        assert (formattedDate.startsWith(yearString));
+        assertEquals("2024-12-13", formattedDate);
     }
+
 }
